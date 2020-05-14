2b48c6aa334cc68199ec225fd62bc76cf5f9f2ca
liushengjie <liushengjie@jd.com>
2020-05-14 09:18:50 +0800

    update

--- src/main/java/com/github/jerome/Application.java
+++ src/main/java/com/github/jerome/Application.java
@@ -4,6 +4,12 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author JeRome
+ * @date 2020-05-13 23:31
+ * @description
+ */
+
 @SpringBootApplication
 @MapperScan("com.github.jerome.mapper")
 public class Application {
--- src/main/java/com/github/jerome/controller/UserMessageController.java
+++ src/main/java/com/github/jerome/controller/UserMessageController.java
@@ -5,12 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * <p>
- *  前端控制器
- * </p>
- *
- * @author 公众号：java思维导图
- * @since 2020-05-14
+ * @author JeRome
+ * @date 2020-05-13 23:31
+ * @description
  */
 @RestController
 @RequestMapping("/m-user-message")
